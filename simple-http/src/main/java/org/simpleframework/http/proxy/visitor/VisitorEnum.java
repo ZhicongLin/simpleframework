e4537c288d1abd46e9c4d4e0430de6426f5b077f
gaonl <gnlyjy@163.com>
2020-07-02 11:48:44 +0800

    添加注释

--- simple-http/src/main/java/org/simpleframework/http/proxy/visitor/VisitorEnum.java
+++ simple-http/src/main/java/org/simpleframework/http/proxy/visitor/VisitorEnum.java
@@ -23,7 +23,12 @@ public enum VisitorEnum {
     private final String key;
     private final Class<?> clazz;
 
-
+    /**
+     * 获取方法参数解析器
+     * @param annClazz
+     * @param <T>
+     * @return
+     */
     public static <T extends Annotation> ParameterVisitor<T> getVisitor(Class<T> annClazz) {
         final VisitorEnum visitorEnum = VisitorEnum.value(annClazz.getName());
         if (visitorEnum == null) {
@@ -50,10 +55,22 @@ public enum VisitorEnum {
         return parameterVisitor;
     }
 
+    /**
+     * 转换参数注解类型
+     * @param annotation
+     * @param clazz
+     * @param <T>
+     * @return
+     */
     public static <T extends Annotation> T ann(Annotation annotation, Class<T> clazz) {
         return (T) annotation;
     }
 
+    /**
+     * 解析器枚举
+     * @param key
+     * @return
+     */
     static VisitorEnum value(String key) {
         final VisitorEnum[] values = VisitorEnum.values();
         for (VisitorEnum value : values) {
